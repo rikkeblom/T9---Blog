640fbb4deda60e23cd3977d6d746be395126787f
Rikke Blom <rikk957v@stud.kea.dk>
2021-04-09 12:04:13 +0200

    functioning comments

--- sendpostid.js
+++ sendpostid.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const artID = urlParams.get("id");
-console.log(artID);
-const url = "https://kea21-7e1e.restdb.io/rest/t9-post/" + artID;
+const url =
+  "https://kea21-7e1e.restdb.io/rest/t9-post/" + artID + "?fetchchildren=true";
 
 fetch(url, {
   method: "GET",
@@ -22,4 +22,58 @@ function showPosts(post) {
   document.querySelector(".postTitle").textContent = post.title;
   document.querySelector(".postAuthor").textContent = `By ${post.username}`;
   document.querySelector(".postText").textContent = post.content;
+  console.log(post.comments);
+
+  post.comments.forEach((comment) => {
+    // grab the template
+    const template = document.querySelector("template").content;
+    //clone the template
+    const copy = template.cloneNode(true);
+    //change the content
+    copy.querySelector(".commentAuthor").textContent = `By ${comment.username}`;
+
+    copy.querySelector(".commentText").textContent = comment.content;
+
+    //grab the parent
+    const parent = document.querySelector(".commentsection div");
+    //apend
+    parent.appendChild(copy);
+  });
+}
+
+//send comments to database
+const form = document.querySelector("form");
+form.addEventListener("submit", userSubmitted);
+
+function userSubmitted(e) {
+  e.preventDefault();
+  const payload = {
+    email: form.elements.email.value,
+    username: form.elements.username.value,
+    content: form.elements.content.value,
+  };
+
+  console.log(payload);
+  document.querySelector("input[type=submit]").disabled = true;
+
+  fetch("https://kea21-7e1e.restdb.io/rest/t9-post/" + artID + "/comments", {
+    method: "POST",
+    headers: {
+      "x-apikey": "602f9e445ad3610fb5bb63d5",
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify(payload),
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      console.log(response);
+      form.elements.email.value = "";
+      form.elements.username.value = "";
+      form.elements.content.value = "";
+      document.querySelector("input[type=submit]").disabled = false;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
